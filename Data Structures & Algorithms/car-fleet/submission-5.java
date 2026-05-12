5b0ea82291d900d6062832cd8d9697b9b0456772
pushingcee <dreamerrusty@gmail.com>
2026-05-12 23:10:02 +0300

    Add: car-fleet - submission-5

---
+++ Data Structures & Algorithms/car-fleet/submission-5.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] pair = new int[n][2];
+        // one place position one place car
+
+        for(int i = 0; i < n; i++) {
+            pair[i][0] = position[i];
+            pair[i][1] = speed[i];
+        }
+
+        Arrays.sort(pair, (a,b) -> b[0] - a[0]);
+        Deque<Double> stack = new ArrayDeque<>();
+        int fleets = 0;
+
+        // s = v * t => t = s / v
+        double timeToTarget = 0;
+        for(int i = 0; i < n; i++) {
+            timeToTarget = (double)(target - pair[i][0]) / pair[i][1]; 
+            
+            if (stack.isEmpty() || timeToTarget > stack.peekFirst()) {
+                stack.addFirst(timeToTarget);
+            }
+        }
+
+        return stack.size();
+    }
+}
