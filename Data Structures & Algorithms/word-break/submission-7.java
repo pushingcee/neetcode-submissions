036510243f63621538c66a0512ece330b9824328
pushingcee <dreamerrusty@gmail.com>
2026-05-18 13:48:01 +0300

    Add: word-break - submission-7

---
+++ Data Structures & Algorithms/word-break/submission-7.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<Integer> memo = new HashSet<>();
+        return dfs(s, wordDict, memo, 0);
+    }
+
+    public boolean dfs(String s, List<String> words, Set<Integer> memo, int start) {
+        if (start > s.length()) {
+            return false;
+        }
+
+        if (start == s.length()) {
+            return true;
+        }
+
+        if (memo.contains(start)) {
+            return false;
+        }
+
+        for (var word : words) {
+            if ( start + word.length() <= s.length() && s.startsWith(word, start)) {
+                if (dfs(s, words, memo, start + word.length())) {
+                    return true;
+                }
+            }
+        }
+
+        memo.add(start);
+        return false;
+    }
+}
