57bc2a47bce156c85f12047ce3d8fa8c5ad7c859
pushingcee <dreamerrusty@gmail.com>
2026-06-08 22:56:48 +0300

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for(var num : nums) sum += num;
+        if(sum % 2 != 0) return false;
+        int target = sum / 2;
+
+        //row element lf nums which we've iteratextends
+        //colums possible values?
+        boolean[][] dp = new boolean[nums.length + 1][target + 1];
+
+        for (int i = 0; i <= nums.length; i++) {
+            dp[i][0] = true;
+        }
+
+        for(int i = 1; i <= nums.length; i++) {
+            for(int j = 1; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+
+        return dp[nums.length][target];
+    }
+}
